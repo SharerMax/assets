160fcd11a9453be978ea65e6bf44bef085ebc3b8
Max Zhao <mdcw1103@gmail.com>
2023-12-02 17:59:26 +0800

    fix: rule generate incorrect

--- clash/build.ts
+++ clash/build.ts
@@ -15,7 +15,7 @@ function isIncludeRule(rule: string) {
 }
 
 function isInvalidRule(rule?: string) {
-    return !rule || !(rule.trim()) || rule.startsWith('#')
+    return !rule || !(rule.trim()) || rule.startsWith('#') || rule.includes('@')
 }
 
 function parse(rules: string[]) {
@@ -24,12 +24,18 @@ function parse(rules: string[]) {
         fullDomain: Array<string>(),
         subdomain: Array<string>(),
     }
-    for (const rule of rules) {
+    for (let rule of rules) {
+        const commentIndex = rule.indexOf('#')
+        if(commentIndex >= 0) {
+            rule = rule.slice(0, commentIndex).trim()
+        }
         if(isInvalidRule(rule)) {
+            console.log("ignore invalid rule: ", rule);
             continue
         } else if(isFullDomainRule(rule)) {
-            result.fullDomain.push(rule.slice(5))
+            result.fullDomain.push(rule.slice(5).trim())
         } else if(isRegexDomainRule(rule)) {
+            console.log('ignore regex rule: ', rule)
         } else if(isIncludeRule(rule)) {
             result.include.push(rule.slice(8))
         } else {
@@ -64,3 +70,14 @@ const yamlObj = {
 yamlObj.payload = yamlObj.payload.concat(result.subdomain)
 yamlObj.payload = yamlObj.payload.concat(result.fullDomain)
 await fs.writeFile('./porn.yaml', yaml.stringify(yamlObj))
+
+const googleResult = await parseUrl(
+  "https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/google"
+);
+const googleYamlObj = {
+  payload: Array<string>(),
+};
+
+googleYamlObj.payload = googleYamlObj.payload.concat(googleResult.subdomain);
+googleYamlObj.payload = googleYamlObj.payload.concat(googleResult.fullDomain);
+await fs.writeFile("./google.yaml", yaml.stringify(googleYamlObj));
